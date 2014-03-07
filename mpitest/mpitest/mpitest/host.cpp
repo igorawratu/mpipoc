25918a2945a478576c059707ca2bd05484d94a06
Sunrise <sunrisewng@gmail.com>
2014-03-07 10:26:48 +0200

    -changed host receives to non-blocking in order to setup for host threading

--- mpitest/mpitest/mpitest/host.cpp
+++ mpitest/mpitest/mpitest/host.cpp
@@ -1,7 +1,7 @@
 #include "host.h"
 
 Host::Host(){
-    mWorkStatus = NOWORK;
+    mWorkStatus = WORK;
 }
 
 Host::~Host(){
@@ -15,6 +15,8 @@ void Host::run(){
     
     MPI_Request* requests = new MPI_Request[totalSlaveProcs - 1];
     int* dat = new int[totalSlaveProcs - 1];
+    for(int k = 0; k < totalSlaveProcs - 1; ++k)
+        dat[k] = 0;
 
     totalWork = completed = 100;
 
@@ -25,36 +27,52 @@ void Host::run(){
         MPI_Irecv(&dat[k - 1], 1, MPI_INT, k, 1, MPI_COMM_WORLD, &requests[k - 1]);
     }
 
+    //launch host worker thread
+
     cout << "processing..." << endl;
 
     //issue work after slave requests
     totalWork = totalWork - totalSlaveProcs + 1;
 
-    while(totalWork){
-        for(uint k = 0; k < 
-    }
-
-    for(int k = 0; k < totalWork; ++k){
-        int slaveRank;
-        MPI_Status status;
-
-        MPI_Recv(&slaveRank, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-        --completed;
-        int workload = rand() % 1000 + 200;
-        cout << "issuing " << workload << " units of work to slave " << slaveRank << endl;
-        MPI_Send(&workload, 1, MPI_INT, slaveRank, 1, MPI_COMM_WORLD);
-
-    }
-
-    cout << "completing..." << endl;
-
-    //wait for slaves to complete the rest of the work
     while(completed){
-        int slaveRank;
-        MPI_Status status;
+        Sleep(100);
 
-        MPI_Recv(&slaveRank, 1, MPI_INT, MPI_ANY_SOURCE, 1, MPI_COMM_WORLD, &status);
-        --completed;
+        //check if a slave has completed work
+        for(int k = 1; k < totalSlaveProcs; ++k){
+            if(dat[k - 1] < 0)
+                continue;
+            
+            MPI_Status status;
+            int received;
+
+            MPI_Test(&requests[k - 1], &received, &status);
+
+            if(received){
+                --completed;
+                if(totalWork){
+                    --totalWork;
+                    int workload = rand() % 1000 + 200;
+                    MPI_Send(&workload, 1, MPI_INT, k, 1, MPI_COMM_WORLD);
+                    MPI_Irecv(&dat[k - 1], 1, MPI_INT, k, 1, MPI_COMM_WORLD, &requests[k - 1]);
+                }
+                else dat[k - 1] = -1;
+            }
+
+        }
+
+        //check if host thread has completed work
+        if(mWorkStatus == NOWORK){
+            --completed;
+            if(totalWork){
+                --totalWork;
+                mWorkStatus = WORK;
+            }
+            else{
+                mWorkStatus = COMPLETE;
+                //join host thread here
+            }
+        }
+
     }
 
     cout << "completing..." << endl;
